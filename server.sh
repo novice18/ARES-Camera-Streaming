ce7b77735b96ed5de36de23be2fd716705d31af5
figment-tree <55850730+figment-tree@users.noreply.github.com>
2020-10-18 07:11:53 +0400

    Delete server.sh

--- server.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! "video/x-raw, width=640, height=480" ! videoconvert ! x264enc bitrate=1000000 speed-preset=1 tune=zerolatency ! rtph264pay pt=96 ! udpsink host=127.0.0.1 port=10000
